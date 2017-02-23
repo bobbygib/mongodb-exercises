fe731d63e07d10f45e3fc8cd14ff500945a80207
bobbygib <bobby@Alisas-MacBook-Pro.local>
2017-02-22 20:25:16 -0700

    2nd commit

--- exercises/exercise_3.js
+++ exercises/exercise_3.js
@@ -1,5 +1,22 @@
 module.exports = function(db) {
 	//What is the title of the movie(s) that was the most checked out?
-	db.collection("movies").distinct('movieId', function(err, data) {
+	db.collection("checkouts").distinct('movieId', function(err, data) {
+		db.things.aggregate([ 
+    {$checkouts:{ id: "$movieId", count: {$size:{"$ifNull":["$movidId",[]]} } }}, 
+    {$sort : {count : -1}}, 
+    {$limit : 1 }
+])
+	db.things.aggregate([
+
+  {$checkouts:{
+    id: "$movieId", count: {$size:{"$ifNull":["$movieId",[]]} }
+  }},
+  {$group: {
+    _id: null, 
+    max: { $max: "$count" }
+  }}
+])
+	console.log(max)
 	console.log ("Exercise 3:\n\tThe movie(s) X, Y -- checked out Z times");
+	});
 };
